0efdebf79920c0688dbece7f53ba07a66fd27f8c
massongui-web <masson.gui@gmail.com>
2026-07-04 20:48:19 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PronoChart from "./PronoChart.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PronoChart />
+  </React.StrictMode>
+);
